f0f2d6e8c95e84f3c1b6e1a2b8db6994217cee89
Andrey Kashin <iiotep9huy@gmail.com>
2013-10-05 21:59:32 +0400

    Nouns added

--- filter.py
+++ filter.py
@@ -13,11 +13,26 @@ for line in inputFile:
 		print(minPrediction[0] + '\t' + minPrediction[1])
 		currentPredictions = []
 	else:
-		line = line.split('\t')
+		line = line.rstrip().split('\t')
 		sourceWord = line[0]
-		prediction = line[1].split('+')[:2]
-		if prediction[1] == '?\n':
+		prediction = line[1].split('+')
+		# print(prediction)
+
+		if prediction[1] != '?' and prediction[1] == 'V':
+			if prediction[3] == '1C':
+				prediction[0] += 'are'
+
+			if prediction[3] == '2C':
+				prediction[0] += 'ere'
+
+			if prediction[3] == '3C':
+				prediction[0] += 'ire'
+
+
+		prediction = prediction[:2]
+		if prediction[1] == '?':
 			prediction[0] = sourceWord
 			prediction[1] = 'N'
+
 		currentPredictions.append((sourceWord, prediction[0] + '+' + prediction[1]))
 
--- process.sh
+++ process.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/sh
 
+foma -l ./italian_vova.foma
+
 echo Building dumb data...
 cat ./data/italian.txt.learn | awk -v OFS='\t' '{ print $1 }' > ./data/italian.txt.learn.clean
 
 echo Building prediction...
-cat ./data/italian.txt.learn.clean | flookup italian.bin > ./data/italian.txt.learn.predict
+cat ./data/italian.txt.test.clean | flookup italian.bin > ./data/italian.txt.learn.predict
 
 echo Filtering prediction...
 ./filter.py <<< ./data/italian.txt.learn.predict > ./data/italian.txt.learn.prediction
 
 echo Evaluating...
 python2 ./data/evaluate.py ./data/italian.txt.learn ./data/italian.txt.learn.prediction
+
+echo Cleaning...
+rm ./data/italian.txt.learn.clean
